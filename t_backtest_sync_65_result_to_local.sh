43a43be4c0026db013fbca366b98b9b29979bb9a
li yong <sunraise2005@gmail.com>
2019-12-30 14:40:06 +0800

    1

--- t_backtest_sync_65_result_to_local.sh
+++ t_backtest_sync_65_result_to_local.sh
@@ -1,64 +1,95 @@
 #!/usr/bin/env bash
-#scp haha_65:/home/ryan/DATA/result/today/*.csv  /home/ryan/DATA/result/today/
-#scp haha_65:/home/ryan/DATA/result/report_2018-11-*.txt /home/ryan/DATA/result/
-
-#scp haha_65:/home/ryan/DATA/result/fenghong_score.csv /home/ryan/DATA/result/
-
-#scp haha_65:/home/ryan/DATA/result/industry_top.csv /home/ryan/DATA/result/
-#scp haha_65:/home/ryan/DATA/result/area_top.csv /home/ryan/DATA/result/
-
-#scp haha_65:/home/ryan/DATA/result/today/announcement.csv /home/ryan/DATA/result/today/
-#scp haha_65:/home/ryan/DATA/result/today/talib_pattern.csv /home/ryan/DATA/result/today/
-#scp haha_65:/home/ryan/DATA/result/today/fundamentals.csv /home/ryan/DATA/result/today/
-#scp haha_65:/home/ryan/DATA/result/today/price_mfi_div.csv /home/ryan/DATA/result/today/
-#scp haha_65:/home/ryan/DATA/result/today/price_rsi_div.csv /home/ryan/DATA/result/today/
-
-#scp haha_65:/home/ryan/DATA/result/today/talib_and_pv_db_buy_filtered.csv /home/ryan/DATA/result/today/
-#scp haha_65:/home/ryan/DATA/result/today/talib_and_pv_db_sell_filtered.csv /home/ryan/DATA/result/today/
-#scp haha_65:/home/ryan/DATA/result/today/talib_and_pv_no_db_filter.csv /home/ryan/DATA/result/today/
-
-#scp haha_65:/home/ryan/DATA/result/latest_fundamental_peg_selected.csv /home/ryan/DATA/result/
-#scp haha_65:/home/ryan/DATA/result/fundamental_peg.csv /home/ryan/DATA/result/
-
-#scp haha_65:/home/ryan/DATA/result/Fundamental_Quarter_Report_2018_1.csv /home/ryan/DATA/result/
-#scp haha_65:/home/ryan/DATA/result/fundamental_peg_2018_1_selected.csv /home/ryan/DATA/result/
-#scp haha_65:/home/ryan/DATA/result/jaqs_quarterly_fundamental.csv /home/ryan/DATA/result/
 
 
-rsync -avz   haha_65:/home/ryan/no_hup_run_exam_ag.log  /home/ryan/
-rsync -avz   haha_65:/home/ryan/no_hup_run_exam_ushk.log  /home/ryan/
+source_host=$1
 
-rsync -avz   haha_65:/home/ryan/DATA/result/*.csv  /home/ryan/DATA/result/
-rsync -avz   haha_65:/home/ryan/DATA/result/*.txt  /home/ryan/DATA/result/
-rsync -az   haha_65:/home/ryan/DATA/result/today/  /home/ryan/DATA/result/today/
+if test $# -eq 0; then
+  echo 'quit';
+  echo "Have to specify \$source_host. Quiting";
+  exit;
+fi;
+
+if [[ ${source_host} == "haha_65" ]]; then
+    echo "haha_65";
+fi
+
+if [[ ${source_host} == "haha_power" ]]; then
+    echo "haha_power";
+fi
 
 
-rsync -avz   haha_66:/hdd/DATA/pickle/Stock_Fundamental/fundamentals/merged/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals/merged/
+#scp ${source_host}:/home/ryan/DATA/result/today/*.csv  /home/ryan/DATA/result/today/
+#scp ${source_host}:/home/ryan/DATA/result/report_2018-11-*.txt /home/ryan/DATA/result/
+
+#scp ${source_host}:/home/ryan/DATA/result/fenghong_score.csv /home/ryan/DATA/result/
+
+#scp ${source_host}:/home/ryan/DATA/result/industry_top.csv /home/ryan/DATA/result/
+#scp ${source_host}:/home/ryan/DATA/result/area_top.csv /home/ryan/DATA/result/
+
+#scp ${source_host}:/home/ryan/DATA/result/today/announcement.csv /home/ryan/DATA/result/today/
+#scp ${source_host}:/home/ryan/DATA/result/today/talib_pattern.csv /home/ryan/DATA/result/today/
+#scp ${source_host}:/home/ryan/DATA/result/today/fundamentals.csv /home/ryan/DATA/result/today/
+#scp ${source_host}:/home/ryan/DATA/result/today/price_mfi_div.csv /home/ryan/DATA/result/today/
+#scp ${source_host}:/home/ryan/DATA/result/today/price_rsi_div.csv /home/ryan/DATA/result/today/
+
+#scp ${source_host}:/home/ryan/DATA/result/today/talib_and_pv_db_buy_filtered.csv /home/ryan/DATA/result/today/
+#scp ${source_host}:/home/ryan/DATA/result/today/talib_and_pv_db_sell_filtered.csv /home/ryan/DATA/result/today/
+#scp ${source_host}:/home/ryan/DATA/result/today/talib_and_pv_no_db_filter.csv /home/ryan/DATA/result/today/
+
+#scp ${source_host}:/home/ryan/DATA/result/latest_fundamental_peg_selected.csv /home/ryan/DATA/result/
+#scp ${source_host}:/home/ryan/DATA/result/fundamental_peg.csv /home/ryan/DATA/result/
+
+#scp ${source_host}:/home/ryan/DATA/result/Fundamental_Quarter_Report_2018_1.csv /home/ryan/DATA/result/
+#scp ${source_host}:/home/ryan/DATA/result/fundamental_peg_2018_1_selected.csv /home/ryan/DATA/result/
+#scp ${source_host}:/home/ryan/DATA/result/jaqs_quarterly_fundamental.csv /home/ryan/DATA/result/
+
+
+rsync -avz   ${source_host}:/home/ryan/no_hup_run_exam_ag.log  /home/ryan/
+rsync -avz   ${source_host}:/home/ryan/no_hup_run_exam_ushk.log  /home/ryan/
+
+rsync -avz   ${source_host}:/home/ryan/DATA/result/*.csv  /home/ryan/DATA/result/
+rsync -avz   ${source_host}:/home/ryan/DATA/result/*.txt  /home/ryan/DATA/result/
+rsync -az   ${source_host}:/home/ryan/DATA/result/today/  /home/ryan/DATA/result/today/
+
+if [[ ${source_host} == "haha_65" ]]; then
+  rsync -avz  haha_66:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals/merged/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals/merged/
+else
+  rsync -avz  ${source_host}:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals/merged/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals/merged/
+fi
+
+
 
 
 
 # sync smaller folders first
-rsync -avz   haha_66:/hdd/DATA/pickle/Stock_Fundamental/fundamentals_2/merged/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/merged/
-rsync -avz   haha_66:/hdd/DATA/pickle/Stock_Fundamental/fundamentals_2/report/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/report/
-rsync -az   haha_66:/hdd/DATA/pickle/Stock_Fundamental/fundamentals_2/source/latest/*.csv  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/latest/
-rsync -az   haha_66:/hdd/DATA/pickle/Stock_Fundamental/fundamentals_2/source/*.csv  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/
-rsync -az   haha_66:/hdd/DATA/pickle/Stock_Fundamental/fundamentals_2/source/individual_per_stock/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/individual_per_stock/
-
+if [[ ${source_host} == "haha_65" ]]; then
+  rsync -avz  haha_66:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/merged/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/merged/
+  rsync -avz  haha_66:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/report/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/report/
+  rsync -az   haha_66:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/latest/*.csv  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/latest/
+  rsync -az   haha_66:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/*.csv  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/
+  rsync -az   haha_66:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/individual_per_stock/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/individual_per_stock/
+else
+  rsync -avz  ${source_host}:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/merged/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/merged/
+  rsync -avz  ${source_host}:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/report/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/report/
+  rsync -az   ${source_host}:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/latest/*.csv  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/latest/
+  rsync -az   ${source_host}:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/*.csv  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/
+  rsync -az   ${source_host}:/home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/individual_per_stock/  /home/ryan/DATA/pickle/Stock_Fundamental/fundamentals_2/source/individual_per_stock/
+fi
 
 ###################################################################
 ###
 ### This file too big, and not necessarily used in haha_brain.
-### Copy to haha_brain only if the haha_65 need be replaced.
-### too big, 6GB. Don't sync jaqs folder, it is directory in remote(haha_65), link in local(haha)
+### Copy to haha_brain only if the ${source_host} need be replaced.
+### too big, 6GB. Don't sync jaqs folder, it is directory in remote(${source_host}), link in local(haha)
 ###################################################################
-#rsync -az   haha_65:/home/ryan/DATA/result/jaqs/ts_all.pickle  /home/ryan/DATA/result/jaqs/
-#rsync -az   haha_65:/home/ryan/DATA/result/jaqs/jaqs_all.pickle  /home/ryan/DATA/result/jaqs/
+#rsync -az   ${source_host}:/home/ryan/DATA/result/jaqs/ts_all.pickle  /home/ryan/DATA/result/jaqs/
+#rsync -az   ${source_host}:/home/ryan/DATA/result/jaqs/jaqs_all.pickle  /home/ryan/DATA/result/jaqs/
 
 
 
 ###################################################################
 #
-#File list that need copy from haha_65 --> haha_brain
+#File list that need copy from ${source_host} --> haha_brain
 #
 ###################################################################
 #
