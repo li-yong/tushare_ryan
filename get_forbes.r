df7c52fe3279de2e831c26ef8f3a75d04130f584
li yong <sunraise2005@gmail.com>
2020-03-24 11:37:07 +0800

    1

--- get_forbes.r
+++ get_forbes.r
@@ -6,7 +6,7 @@ library(magrittr)
 nlst <- list( "30 Under 30", "Powerful Women",         "Celebrities", "MLB Valuations", "NASCAR Valuations",         "NFL Valuations", "NBA Valuations", "NHL Valuations",         "Soccer Valuations", "Top Colleges", "Top Business Schools",         "Innovative Companies", "Small Companies", "Best Employers",         "Largest Private Companies", "Asia 200", "Asia Fab 50",         "Most Promising Companies", "Powerful Brands", "Growth Companies",         "Best Employers", "Global 2000", "Best Countries for Business",         "Best Cities for Business", "Best States for Business",         "Best Small Cities for Business", "Top VCs", "Athletes",  "Forbes 400", "Billionaires", "Self Made Women",         "Richest in Tech", "Hong Kong 50", "Australia 50", "China 50",         "Taiwan 50", "India 50", "Japan 50", "Africa 50", "Korea 50",         "Malaysia 50", "Philippines 50", "Singapore 50", "Indonesia 50", "Thailand 50", "Richest Families","Powerful People")
 
 
-year = 2018
+year = 2019
 
 
 
--- t_daily_w_shape.py
+++ t_daily_w_shape.py
@@ -31,43 +31,69 @@ from optparse import OptionParser
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m_%d %H:%M:%S', level=logging.DEBUG)
 
 
+def _diff(d1,d2):
+    return( (d1-d2)**2*100/(d1+d2))
+
+def equal(d1,d2,threshold=3):
+    if _diff(d1,d2)<=threshold:
+        return(True)
+    else:
+        return(False)
+
+def great(d1,d2, threshold = 3):
+    if (d1>d2) and _diff(d1,d2)>=threshold:
+        return(True)
+    else:
+        return(False)
+
+def less(d1,d2,threshold = 3):
+    if(d1<d2) and _diff(d1,d2)>=threshold:
+        return(True)
+    else:
+        return(False)
+
+
+
+
 # past < t3 < t2 < t1 < now. dx is the price of tx.
-def get_trend(d1,d2,d3):
-    if (d3 == d2):
-        if d2 == d1:
+def get_trend(d3,d2,d1):
+    trend = 'none'
+
+    if d3>d2 and d2>d1:
+        trend='down'
+        return(trend)
+
+    if d3<d2 and d2<d1:
+        trend='up'
+        return(trend)
+
+    if equal(d3, d2, threshold=2):
+        if equal(d2,d1,threshold=2):
             trend = 'none'
-        elif d2 > d1:
+        elif great(d2, d1,threshold=0.5):
             trend = 'down'
-        elif d2 < d1:
+        elif less(d2,d1, threshold=0.5):
             trend = 'up'
 
-    elif d3 > d2:
-        if (d2 >= d1):
+    elif great(d3,d2,threshold=2):
+        if great(d2, d1,threshold=0.5):
             trend="down"
-        elif d2 < d1:
+        elif less(d2, d1,threshold=2):
             trend="down_up"
-    elif d3 < d2:
-        if (d2 <= d1):
+    elif less(d3,d2,threshold=2):
+        if less(d2,d1,threshold=0.5):
             trend="up"
-        elif d2 > d1:
+        elif great(d2,d1,threshold=2):
             trend="up_down"
 
     return(trend)
 
 
-def _w_shape(csv_f, period):
+
+def w_shape(csv_f, period):
     csv_f = '/home/ryan/DATA/DAY_Global/AG/SH600519.csv' #ryan debug
     period = 'D'
 
-    rtn = {
-        "reason":[''],
-        "strength":[''],
-        "action":[''],
-        "code":[''],
-        "date":[''],
-        "p_cur":[''],
-        "p_w_head":[''],
-    }
 
     if not os.path.exists(csv_f):
         logging.info('file not exist. '+csv_f)
@@ -80,21 +106,6 @@ def _w_shape(csv_f, period):
     df = pd.read_csv(csv_f, converters={'code': str}, header=None, skiprows=1,
                      names=['code', 'date', 'open', 'high', 'low', 'close', 'volume', 'amount', 'tnv'])
 
-    if df.__len__() < 100:
-        logging.info('file less than 100 records. '+csv_f)
-        return(rtn)
-
-    this_code = df.iloc[0]['code'] #'SH603999'
-    this_date='' #monthly period, end date of the month.
-    this_reason=''
-    this_strength=''
-    this_action=''
-
-    # logging.info(tabulate.tabulate(df[-20:], headers='keys', tablefmt='psql'))  #ryan debug
-    # logging.info(tabulate.tabulate(df_monthly_s[-20:], headers='keys', tablefmt='psql'))
-
-    df = df[-100:] #last 3 months.
-
 
     if period == "M":
         df_period = finlib.Finlib().daily_to_monthly_bar(df_daily=df)['df_monthly']
@@ -104,18 +115,55 @@ def _w_shape(csv_f, period):
         df['date'] = pd.to_datetime(df['date'], format="%Y-%m-%d")
         df_period = df
 
-    #df_sorted = df.sort_values(by='close')
+    len = df_period.__len__()
+    for i in range(len):
+        df_new = df_period[len-i-100: len -i]
+        logging.info("Verifying "+str(df_new[-1:].date.values[0]))
+        rtn = _w_shape(df_new)
+        if rtn['reason'][0]=="NOT_MATCH":
+            pass
+        elif rtn['strength'][0]<=0:
+            pass
+        else:
+            print("HIT!!!")
+            print(rtn)
+            #exit(0)
 
-    p = df.iloc[-1].close
+def _w_shape(df):
+
+    rtn = {
+        "reason":[''],
+        "strength":[''],
+        "action":[''],
+        "code":[''],
+        "date":[''],
+        "p_cur":[''],
+        "p_w_head":[''],
+    }
+
+    if df.__len__() < 100:
+        logging.info('file less than 100 records. '+csv_f)
+        return(rtn)
+
+    this_code = df.iloc[0]['code'] #'SH603999'
+    this_date=df.iloc[-1]['date'].strftime("%Y-%m-%d") #monthly period, end date of the month.
+    this_reason=''
+    this_strength=0
+    this_action=''
+
+    # logging.info(tabulate.tabulate(df[-20:], headers='keys', tablefmt='psql'))  #ryan debug
+    # logging.info(tabulate.tabulate(df_monthly_s[-20:], headers='keys', tablefmt='psql'))
+
     stage = "null"
-
+    d0=d1=d2=d3=d4=d5=d6=d7=d8="null"
+    p0=p1=p2=p3=p4=p5=p6=p7=p8=0
 
     df = df.reset_index().drop('index', axis=1)
     stage == 'null'
 
     for i in reversed(range(df.__len__())):
         if i <= 3:
-            return
+            break
 
         r1=df.loc[i]
         r2=df.loc[i-1]
@@ -123,11 +171,216 @@ def _w_shape(csv_f, period):
 
         trend = get_trend(r3.close, r2.close, r1.close)
 
-        logging.info(trend+" "+str(r1.date)+str(r1.close)+ ". v_point:"+str(r2.date)+str(r2.close)+" "+str(r3.date)+str(r3.close))
+        #logging.info(trend+" "+str(r1.date)+str(r1.close)+ ". v_point:"+str(r2.date)+str(r2.close)+" "+str(r3.date)+str(r3.close))
+
+        if trend == 'up':
+            if stage == 'null':
+                p0 = r1.close
+                d0 = r1.date.strftime("%Y-%m-%d")
+                logging.info("init s0"+", middle point:"+str(r1.date)+" "+ str( r1.close))
+                stage = "s0"
+                continue
+
+            if stage in ["s0","s2","s4","s6","s8"]:
+                logging.info("moving up on "+stage)
+                continue
+
+        if trend == 'down':
+            if stage == 'null':
+                this_reason = "NOT_MATCH"
+                logging.info("not match")
+                break
+
+            if stage in ["s1","s3","s5","s7"]:
+                logging.info("moving down on "+stage)
+                continue
+
+
+        if trend == 'up_down':
+            if stage == 'null':
+                logging.info("not match")
+                this_reason = "NOT_MATCH"
+                break
+
+            if stage in ["s1"]:
+                logging.info("spot p2 in "+stage+", middle point:"+str(r2.date)+" "+ str(r2.close))
+                p2 = r2.close
+                d2 = r2.date.strftime("%Y-%m-%d")
+                stage = "s2"
+                continue
+
+            if stage in ["s3"]:
+                logging.info("spot p4 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
+                p4 = r2.close
+                d4 = r2.date.strftime("%Y-%m-%d")
+                stage = "s4"
+                continue
+
+
+
+            if stage in ["s5"]:
+                logging.info("spot p6 in "+stage+", middle point:"+str(r2.date)+" "+ str(r2.close))
+                p6 = r2.close
+                d6 = r2.date.strftime("%Y-%m-%d")
+                stage = "s6"
+                continue
+
+            if stage in ["s7"]:
+                logging.info("spot p8 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
+                p8 = r2.close
+                d8 = r2.date.strftime("%Y-%m-%d")
+                stage = "s8"
+                continue
+
+        if trend == 'down_up':
+            if stage == 'null':
+                logging.info("not match")
+                this_reason = "NOT_MATCH"
+                break
+
+            if stage in ["s0"]:
+                logging.info("spot p1 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
+                stage = "s1"
+                p1 = r2.close
+                d1 = r2.date.strftime("%Y-%m-%d")
+                continue
+
+            if stage in ["s2"]:
+                logging.info("spot p3 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
+                p3 = r2.close
+                d3 = r2.date.strftime("%Y-%m-%d")
+                stage = "s3"
+
+                if (p3<=p1) and (p2<=p0):
+                    this_strength = 1
+                    continue
+                else:
+                    break
+
+
+            if stage in ["s4"]:
+                logging.info("spot p5 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
+                stage = "s5"
+                p5 = r2.close
+                d5 = r2.date.strftime("%Y-%m-%d")
+                if (p5 < p3) and (p4 < p2):
+                    this_strength = 2
+                    continue
+                else:
+                    break
+
+            if stage in ["s6"]:
+                logging.info("spot p7 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
+                p7 = r2.close
+                d7 = r2.date.strftime("%Y-%m-%d")
+                stage = "s7"
+                if (p7 < p5) and (p8 < p6):
+                    this_strength = 3
+                    continue
+                else:
+                    break
+
+
+    rtn = {
+        "reason":[this_reason],
+        "strength":[this_strength],
+        "action":[this_action],
+        "code":[this_code],
+        "date":[this_date],
+        "date_0":[d0,p0],
+        "date_1":[d1,p1],
+        "date_2":[d2,p2],
+        "date_3":[d3,p3],
+        "date_4":[d4,p4],
+        "date_5":[d5,p5],
+        "date_6":[d6,p6],
+        "date_7":[d7,p7],
+        "date_8":[d8,p8],
+    }
+
+    return(rtn)
+
+
+def _w_shape_del(df):
+
+    rtn = {
+        "reason":[''],
+        "strength":[''],
+        "action":[''],
+        "code":[''],
+        "date":[''],
+        "p_cur":[''],
+        "p_w_head":[''],
+    }
+
+    if df.__len__() < 100:
+        logging.info('file less than 100 records. '+csv_f)
+        return(rtn)
+
+    this_code = df.iloc[0]['code'] #'SH603999'
+    this_date=df.iloc[-1]['date'].strftime("%Y-%m-%d") #monthly period, end date of the month.
+    this_reason=''
+    this_strength=0
+    this_action=''
+
+    # logging.info(tabulate.tabulate(df[-20:], headers='keys', tablefmt='psql'))  #ryan debug
+    # logging.info(tabulate.tabulate(df_monthly_s[-20:], headers='keys', tablefmt='psql'))
+
+    stage = "null"
+    d0=d1=d2=d3=d4=d5=d6=d7=d8="null"
+    p0=p1=p2=p3=p4=p5=p6=p7=p8="null"
+
+    df = df.reset_index().drop('index', axis=1)
+    stage == 'null'
+
+    df = df.sort_values(by='close')
+
+
+    pass
+    hd0=df.iloc[-1]['date'].date().strftime('%Y-%m-%d')
+    hd1=df.iloc[-2]['date'].date().strftime('%Y-%m-%d')
+    hd3=df.iloc[-3]['date'].date().strftime('%Y-%m-%d')
+    hd4=df.iloc[-4]['date'].date().strftime('%Y-%m-%d')
+
+    hp0=df.iloc[-1]['close']
+    hp1=df.iloc[-2]['close']
+    hp3=df.iloc[-3]['close']
+    hp4=df.iloc[-4]['close']
+
+
+    l0=df.iloc[1]['date'].date().strftime('%Y-%m-%d')
+    l1=df.iloc[2]['date'].date().strftime('%Y-%m-%d')
+    l3=df.iloc[3]['date'].date().strftime('%Y-%m-%d')
+    l4=df.iloc[4]['date'].date().strftime('%Y-%m-%d')
+
+    lp0=df.iloc[-1]['close']
+    lp1=df.iloc[-2]['close']
+    lp3=df.iloc[-3]['close']
+    lp4=df.iloc[-4]['close']
+
+    pass
+
+
+
+
+
+
+    for i in reversed(range(df.__len__())):
+        if i <= 3:
+            break
+
+        r1=df.loc[i]
+        r2=df.loc[i-1]
+        r3=df.loc[i-2]
+
+        trend = get_trend(r3.close, r2.close, r1.close)
+
+        #logging.info(trend+" "+str(r1.date)+str(r1.close)+ ". v_point:"+str(r2.date)+str(r2.close)+" "+str(r3.date)+str(r3.close))
 
         if trend == 'up':
             if stage == 'null':
                 p0 = r2.close
+                d0 = r2.date.strftime("%Y-%m-%d")
                 logging.info("init s0"+", middle point:"+str(r2.date)+" "+ str( r2.close))
                 stage = "s0"
                 continue
@@ -138,6 +391,7 @@ def _w_shape(csv_f, period):
 
         if trend == 'down':
             if stage == 'null':
+                this_reason = "NOT_MATCH"
                 logging.info("not match")
                 break
 
@@ -149,51 +403,86 @@ def _w_shape(csv_f, period):
         if trend == 'up_down':
             if stage == 'null':
                 logging.info("not match")
+                this_reason = "NOT_MATCH"
                 break
-
-            if stage in ["s0"]:
-                logging.info("spot p1 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
-                p1 = r2.close
-                stage = "s1"
-                continue
-
 
             if stage in ["s1"]:
                 logging.info("spot p2 in "+stage+", middle point:"+str(r2.date)+" "+ str(r2.close))
                 p2 = r2.close
+                d2 = r2.date.strftime("%Y-%m-%d")
                 stage = "s2"
-                continue
-
-            if stage in ["s2"]:
-                logging.info("spot p3 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
-                p3 = r2.close
-                stage = "s3"
                 continue
 
             if stage in ["s3"]:
                 logging.info("spot p4 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
                 p4 = r2.close
+                d4 = r2.date.strftime("%Y-%m-%d")
                 stage = "s4"
                 continue
 
 
+
+            if stage in ["s5"]:
+                logging.info("spot p6 in "+stage+", middle point:"+str(r2.date)+" "+ str(r2.close))
+                p6 = r2.close
+                d6 = r2.date.strftime("%Y-%m-%d")
+                stage = "s6"
+                continue
+
+            if stage in ["s7"]:
+                logging.info("spot p8 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
+                p8 = r2.close
+                d8 = r2.date.strftime("%Y-%m-%d")
+                stage = "s8"
+                continue
+
         if trend == 'down_up':
             if stage == 'null':
                 logging.info("not match")
+                this_reason = "NOT_MATCH"
                 break
 
             if stage in ["s0"]:
                 logging.info("spot p1 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
                 stage = "s1"
                 p1 = r2.close
+                d1 = r2.date.strftime("%Y-%m-%d")
                 continue
 
             if stage in ["s2"]:
                 logging.info("spot p3 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
                 p3 = r2.close
+                d3 = r2.date.strftime("%Y-%m-%d")
                 stage = "s3"
-                continue
 
+                if (p3<=p1) and (p2<=p0):
+                    this_strength = 1
+                    continue
+                else:
+                    break
+
+
+            if stage in ["s4"]:
+                logging.info("spot p5 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
+                stage = "s5"
+                p5 = r2.close
+                d5 = r2.date.strftime("%Y-%m-%d")
+                if (p5 < p3) and (p4 < p2):
+                    this_strength = 2
+                    continue
+                else:
+                    break
+
+            if stage in ["s6"]:
+                logging.info("spot p7 in "+stage+", middle point:"+str(r2.date)+" "+ str( r2.close))
+                p7 = r2.close
+                d7 = r2.date.strftime("%Y-%m-%d")
+                stage = "s7"
+                if (p7 < p5) and (p8 < p6):
+                    this_strength = 3
+                    continue
+                else:
+                    break
 
 
     rtn = {
@@ -202,14 +491,15 @@ def _w_shape(csv_f, period):
         "action":[this_action],
         "code":[this_code],
         "date":[this_date],
-        "k1":[round(_k1,1)],
-        "d1":[round(_d1,1)],
-        "j1":[round(_j1,1)],
-
-        "k2": [round(_k2,1)],
-        "d2": [round(_d2,1)],
-        "j2": [round(_j2,1)],
-
+        "date_0":[d0,p0],
+        "date_1":[d1,p1],
+        "date_2":[d2,p2],
+        "date_3":[d3,p3],
+        "date_4":[d4,p4],
+        "date_5":[d5,p5],
+        "date_6":[d6,p6],
+        "date_7":[d7,p7],
+        "date_8":[d8,p8],
     }
 
     return(rtn)
@@ -243,7 +533,7 @@ def main():
     period = options.period_f
     analyze_f = options.analyze_f
 
-    _w_shape(csv_f="",period='D')
+    w_shape(csv_f="",period='D')
 
 
 
