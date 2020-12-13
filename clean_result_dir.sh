6abb3d820bc030b1b883bfe4edb55c940d7b199f
li yong <sunraise2005@gmail.com>
2020-12-14 07:55:58 +0800

    1

--- clean_result_dir.sh
+++ clean_result_dir.sh
@@ -8,10 +8,18 @@ rm -f curv_plot/SZ*.png
 rm -f curv_plot/SH*.png
 rm -f curv_plot/{A..Z}*.png
 rm -f curv_plot/{0..9}*.png
-rm curv_plot/* 
+rm -f curv_plot/* 
 rm -f report_20201*.cv
 rm -f report_20201*
 rm -f wei_pan_la_sheng/*
 rm -f today/*
 rm -f fib_plot/*
 
+
+
+cd /home/ryan/DATA/pickle/Stock_Fundamental/akshare/source
+rm -f ag_spot_*.csv
+rm -f ag_kcb_spot_*.csv
+rm -f wei_pan_la_sheng_*.csv
+
+rm -rf /home/ryan/DATA/pickle/Stock_Fundamental/ak_share
