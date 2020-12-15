614acafcc03b29a4aeb6a759f32397b5cbd37cf9
li yong <sunraise2005@gmail.com>
2020-12-15 23:00:29 +0800

    1

--- t_daily_run_1_update_data.sh
+++ t_daily_run_1_update_data.sh
@@ -234,8 +234,9 @@ if [ $full_or_daily == "DAILY" ]; then
   python t_daily_fundamentals_2.py --fetch_pro_fund --fast_fetch  #pro fundation tables. 10 tables.
   python t_daily_fundamentals_2.py --fetch_change_name
 
-  #fetch lastest quarter fund data, all stocks
-  python t_daily_fundamentals_2.py --fetch_pro_fund --fast_fetch --force_run
+  #`fetch lastest quarter fund data, all stocks
+  # comment this as in Q3, audit is empty and always fetch everytime as is_cached return False
+  #python t_daily_fundamentals_2.py --fetch_pro_fund --fast_fetch --force_run
 fi
 
 
