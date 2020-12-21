8225e10a71d3e77aeed38bc0f71a16837ffe2cc7
li yong <sunraise2005@gmail.com>
2020-12-22 00:53:10 +0800

    1

--- t_daily_hs300_candidate.py
+++ t_daily_hs300_candidate.py
@@ -57,10 +57,9 @@ if __name__ == '__main__':
 
     my_hs300 = finlib.Finlib()._df_sub_by_code(df=df_mktcap, df_sub=df_amt.tail(math.ceil(df_amt.__len__() / 2)),
                                                byreason="daily average amount less than 50% stocks.")
-    my_hs300 = my_hs300.head(300)[['code', 'name', 'total_mv']]
+    my_hs300 = my_hs300.head(300)[['code']]
 
-    df_merged = my_hs300.merge(df_hs300, indicator=True, how='outer')
-    df_merged.rename(columns={'left_only': 'new_candidate', 'right_only': 'to_be_replaced'}, inplace=True)
+    df_merged = my_hs300.merge(df_hs300, indicator=True, on='code', how='outer').reset_index().drop('index', axis=1)
     len_merged = df_merged.__len__()
     df_merged.to_csv(hs300_candiate_csv, encoding='UTF-8', index=False)
     logging.info("saved " + hs300_candiate_csv + " len " + str(len_merged))
@@ -68,17 +67,22 @@ if __name__ == '__main__':
     df_both = df_merged[df_merged['_merge'] == "both"]
     len_both = df_both.__len__()
 
-    df_myonly = df_merged[df_merged['_merge'] == "new_candidate"]
-    len_myonly = df_myonly.__len__()
-    logging.info(
-        str(len_myonly) + " out of " + str(len_both) + " in my hs300, they possible will be added to hs300 next time")
-    logging.info(finlib.Finlib().pprint(df=df_myonly))
+    df_myonly = df_merged[df_merged['_merge'] == "left_only"].reset_index().drop('index', axis=1)
+    df_myonly['new_candidate'] = True
 
-    df_hs300only = df_merged[
-        df_merged['_merge'] == "to_be_replaced"]  # possible will be removed from hs300 index next time
+    len_myonly = df_myonly.__len__()
+    logging.info( str(len_myonly) + " out of " + str(len_both) + " in my hs300, they possible will be added to hs300 next time")
+    logging.info(finlib.Finlib().pprint(df=df_myonly.head(2)))
+
+
+    df_hs300only = df_merged[df_merged['_merge'] == "right_only"].reset_index().drop('index', axis=1)  # possible will be removed from hs300 index next time
+    df_hs300only['to_be_removed'] = True
     len_hs300only = df_hs300only.__len__()
     logging.info(str(len_hs300only) + " out of " + str(
         len_both) + " in offical hs300, they possible will be removed from hs300 next time")
-    logging.info(finlib.Finlib().pprint(df=df_hs300only))
+    logging.info(finlib.Finlib().pprint(df=df_hs300only.head(2)))
+
+    logging.info("result saved to " + hs300_candiate_csv + " len " + str(len_merged))
+
 
     exit(0)
