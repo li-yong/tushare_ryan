fd555838a8280b47c7771dcec50516778db1f4bb
li yong <sunraise2005@gmail.com>
2021-01-05 06:37:13 +0800

    1

--- t_daily_sync_from_ds.sh
+++ t_daily_sync_from_ds.sh
@@ -33,7 +33,7 @@ ssh haha_data_source "cd ~/tushare_ryan;  git pull"
 rsync -avz /home/ryan/DATA/pickle/INDEX_US_HK/ ${source_host}:/home/ryan/DATA/pickle/INDEX_US_HK
 
 #TradingView are download to haha_brain manually on Chrome.
-rsync -avz /home/ryan/DATA/pickle/Stock_Fundamental/TradingView ${source_host}:/home/ryan/DATA/pickle/Stock_Fundamental/TradingView
+rsync -avz /home/ryan/DATA/pickle/Stock_Fundamental/TradingView/ ${source_host}:/home/ryan/DATA/pickle/Stock_Fundamental/TradingView
 #rsync -avz /home/ryan/DATA/result/*.csv ${source_host}:/home/ryan/DATA/result/
 #rsync -avz /home/ryan/DATA/result/*.txt ${source_host}:/home/ryan/DATA/result/
 #rsync -avz /home/ryan/DATA/result/today ${source_host}:/home/ryan/DATA/result/
