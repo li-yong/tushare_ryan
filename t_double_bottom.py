35e2c74c2b3b9e10f1cc516daa297209ce26a0a0
li yong <sunraise2005@gmail.com>
2021-02-21 00:01:26 +0800

    1

--- t_double_bottom.py
+++ t_double_bottom.py
@@ -338,7 +338,7 @@ def draw_a_stock(df, code, name, show_fig_f=False, save_fig_f=False, min_sample=
 
     if save_fig_f:
         if rtn_dict['hit']:
-            fn = dir+"/" + code + "_" + name + "_" + the_day + "_double_bottom_hitted.png"
+            fn = dir+"/" + code + "_" + name + "_" + the_day + "_double_bottom_"+rtn_dict['reason'][0:200]+".png"
         # else:
         #     fn = dir+"/" + code + "_" + name + "_" + the_day + ".png" # commented as too many png
 
