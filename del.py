4b2abbff8d02bc2c4f209801431cddebcd66e9b3
li yong <sunraise2005@gmail.com>
2021-06-04 11:46:45 +0800

    1

--- del.py
+++ del.py
@@ -63,50 +63,56 @@ def roe_pe():
 
 
 def garbage_increase():
-    # roe_pe()
+    startD = 20210602
+    endD = 20210603
 
-    dir = "/home/ryan/DATA/result/garbage"
-
-    startD = 20210601
-    endD = 20210602
-
-    all_files = glob.glob(dir+"/latest_*.csv")
-
+    #prepare amount df
     df_amount = finlib.Finlib().get_last_n_days_stocks_amount(ndays=5, dayS=str(startD), dayE=str(endD), daily_update=None, short_period= True, debug=False, force_run=False)
     df_close_start =  df_amount[df_amount['date']==int(startD)]
     df_close_end =  df_amount[df_amount['date']==int(endD)]
-    df_close_amount =  pd.merge(df_close_start[['code','date','close','amount']],df_close_end[['code','date','close','amount']], on='code',how='inner', suffixes=('_dayS','_dayE'))
-    df_close_amount['amount_increase'] = round((df_close_amount['amount_dayE'] - df_close_amount['amount_dayS']) *100.0 /  df_close_amount['amount_dayS'],2)
+    df_amount =  pd.merge(df_close_start[['code','date','close','amount']],df_close_end[['code','date','close','amount']], on='code',how='inner', suffixes=('_dayS','_dayE'))
+    df_amount['amount_increase'] = round((df_amount['amount_dayE'] - df_amount['amount_dayS']) *100.0 /  df_amount['amount_dayS'],2)
 
+    #prepare close df
     df_basic = finlib.Finlib().get_last_n_days_daily_basic(ndays=10,dayS=None,dayE=None,daily_update=None,debug=False, force_run=False)
     df_close_start = df_basic[df_basic['trade_date']==int(startD)]
     df_close_end = df_basic[df_basic['trade_date']==int(endD)]
     df_close =  pd.merge(df_close_start[['ts_code','close','trade_date']],df_close_end[['ts_code','close','trade_date']], on='ts_code',how='inner', suffixes=('_dayS','_dayE'))
-
     df_close = finlib.Finlib().ts_code_to_code(df=df_close)
     df_close = finlib.Finlib().add_stock_name_to_df(df=df_close)
 
-    #
-    # csv = "latest_ST_stock.csv"
-    # df = pd.read_csv(dir+"/"+csv)
-    #
-    for csv in all_files:
-        logging.info("reading "+csv)
+    #calculate HS300
+    _get_avg_chg_of_code_list(list_name="HS300", df_code_column_only=pd.read_csv("/home/ryan/DATA/pickle/Stock_Fundamental/WuGuiLiangHua/SH000300.csv"), df_close=df_close, df_amount=df_amount)
+    _get_avg_chg_of_code_list(list_name="ZhongZhen100", df_code_column_only=pd.read_csv("/home/ryan/DATA/pickle/Stock_Fundamental/WuGuiLiangHua/SH000903.csv"), df_close=df_close, df_amount=df_amount)
+    _get_avg_chg_of_code_list(list_name="ZhongZhen500", df_code_column_only=pd.read_csv("/home/ryan/DATA/pickle/Stock_Fundamental/WuGuiLiangHua/SH000905.csv"), df_close=df_close, df_amount=df_amount)
+    _get_avg_chg_of_code_list(list_name="ShenZhenChenZhi", df_code_column_only=pd.read_csv("/home/ryan/DATA/pickle/Stock_Fundamental/WuGuiLiangHua/SZ399001.csv"), df_close=df_close, df_amount=df_amount)
+    _get_avg_chg_of_code_list(list_name="ShenZhen100", df_code_column_only=pd.read_csv("/home/ryan/DATA/pickle/Stock_Fundamental/WuGuiLiangHua/SZ399330.csv"), df_close=df_close, df_amount=df_amount)
+    _get_avg_chg_of_code_list(list_name="KeJiLongTou", df_code_column_only=pd.read_csv("/home/ryan/DATA/pickle/Stock_Fundamental/WuGuiLiangHua/CSI931087.csv"), df_close=df_close, df_amount=df_amount)
+
+    #calculate garbage stocks close/amount increase
+    for csv in glob.glob("/home/ryan/DATA/result/garbage/latest_*.csv"):
+        # logging.info("reading "+csv)
         df = pd.read_csv(csv)
-
-
-
-        df_2 = pd.merge(df[['code']],df_close[['code','name','close_dayS','trade_date_dayS','close_dayE','trade_date_dayE']], on='code',how='inner')
-        df_2 = pd.merge(df_2,df_close_amount[['code','amount_increase']], on='code',how='inner')
-        df_2['close_delta'] = round((df_2['close_dayE']-df_2['close_dayS'])*100.0/df_2['close_dayS'],2)
-        chg_mean_perc_close = round(df_2['close_delta'].mean(),2)
-        chg_mean_perc_amt = round(df_2['amount_increase'].mean(),2)
-
-        print(str(df_close.trade_date_dayS.iloc[0])+"->"+str(df_close.trade_date_dayE.iloc[0])+" len "+str(df_2.__len__())+" " +csv+ ",  change average close "+str(chg_mean_perc_close)+"%,  change average amount "+str(chg_mean_perc_amt)+"%")
+        _get_avg_chg_of_code_list(list_name=csv, df_code_column_only=df[['code']], df_close=df_close, df_amount=df_amount)
 
     print("HAHA")
     exit(0)
 
+
+def _get_avg_chg_of_code_list(list_name, df_code_column_only, df_close,df_amount):
+    df_2 = pd.merge(df_code_column_only[['code']],
+                    df_close[['code', 'name', 'close_dayS', 'trade_date_dayS', 'close_dayE', 'trade_date_dayE']],
+                    on='code', how='inner')
+    df_2 = pd.merge(df_2, df_amount[['code', 'amount_increase']], on='code', how='inner')
+    df_2['close_delta'] = round((df_2['close_dayE'] - df_2['close_dayS']) * 100.0 / df_2['close_dayS'], 2)
+    chg_mean_perc_close = round(df_2['close_delta'].mean(), 2)
+    chg_mean_perc_amt = round(df_2['amount_increase'].mean(), 2)
+
+    print(str(df_close.trade_date_dayS.iloc[0]) + "->" + str(df_close.trade_date_dayE.iloc[0]) + " len " + str(
+        df_2.__len__()) + " " + list_name + ",  change average close " + str(
+        chg_mean_perc_close) + "%,  change average amount " + str(chg_mean_perc_amt) + "%")
+
+    return()
 
 
 
