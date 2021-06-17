ea89466c0eb30b5f0efa42062a56d4714f6e4dac
li yong <sunraise2005@gmail.com>
2021-06-17 18:25:58 +0800

    1

--- t_daily_sync_from_ds.sh
+++ t_daily_sync_from_ds.sh
@@ -46,6 +46,7 @@ rsync -avz /home/ryan/DATA/pickle/Stock_Fundamental/TradingView/ ${source_host}:
 #v: sync recursivly
 rsync -avz ${source_host}:/home/ryan/DATA/pickle/Stock_Fundamental/akshare/  /home/ryan/DATA/pickle/Stock_Fundamental/akshare/
 
+rsync -avz ${source_host}:/home/ryan/DATA/pickle/Stock_Fundamental/WuGuiLiangHua/ /home/ryan/DATA/pickle/Stock_Fundamental/WuGuiLiangHua/
 
 
 ######################################
