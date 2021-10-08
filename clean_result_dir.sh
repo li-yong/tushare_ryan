b16cd0b980ec0d6d271753c8608ea66ddafe1bb5
li yong <sunraise2005@gmail.com>
2021-10-08 22:01:51 +0800

    1

--- clean_result_dir.sh
+++ clean_result_dir.sh
@@ -8,7 +8,10 @@ rm -f curv_plot/SZ*.png
 rm -f curv_plot/SH*.png
 rm -f curv_plot/{A..Z}*.png
 rm -f curv_plot/{0..9}*.png
-rm -f curv_plot/* 
+rm -f curv_plot/*
+
+rm -f zigzag_div/*
+
 rm -f report_202101*
 rm -f report_new_dev_B_202101*
 rm -f report_new_dev_S_202101*
@@ -19,20 +22,20 @@ rm -f average_daily_mktcap_sorted*
 rm -f average_daily_amount_sorted*
 rm -f latest_daily_basic_*
 
-rm -f wei_pan_la_sheng/*20210[1-6]*
+rm -f wei_pan_la_sheng/*20210[1-9]*
 rm -f today/*
 rm -f fib_plot/*
 
 
 
-rm -f report*20210[1-8]*
-rm -f daily_basic_20210[1-8]*
+rm -f report*20210[1-9]*
+rm -f daily_basic_20210[1-9]*
 rm -f garbage/*
-rm -fr result_new_dev_B/*20210[1-8]*
-rm -fr result_new_dev_S/*20210[1-8]*
+rm -fr result_new_dev_B/*20210[1-9]*
+rm -fr result_new_dev_S/*20210[1-9]*
 
-rm -f wei_pan_la_sheng/*20210[1-8]*
-rm -f basic_summary/*20210[1-8]*
+rm -f wei_pan_la_sheng/*20210[1-9]*
+rm -f basic_summary/*20210[1-9]*
 rm -f basic_summary/*2020*
 
 
