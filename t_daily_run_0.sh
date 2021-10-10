ae98c1dbda76e7274062d312f072a0d26af0d1ea
li yong <sunraise2005@gmail.com>
2021-10-10 10:19:31 +0800

    1

--- t_daily_run_0.sh
+++ t_daily_run_0.sh
@@ -6,11 +6,9 @@
 cd /home/ryan/tushare_ryan
 git pull
 
-
-bash -x t_daily_selected_f_e.sh FETCH
-bash -x t_daily_selected_f_e.sh EXAM
-
-
+#
+#bash -x t_daily_selected_f_e.sh FETCH
+#bash -x t_daily_selected_f_e.sh EXAM
 
 bash -x t_daily_run_1_update_data.sh DAILY
 bash -x t_daily_run_2_exam.sh DAILY
