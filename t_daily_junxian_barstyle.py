89b124cbc21c09b23de392f6c285d6a5deb89d37
li yong <sunraise2005@gmail.com>
2021-11-23 04:46:22 +0800

    1

--- t_daily_junxian_barstyle.py
+++ t_daily_junxian_barstyle.py
@@ -399,6 +399,9 @@ def main():
         elif period=='M':
             df = finlib.Finlib().daily_to_monthly_bar(df_daily=df)['df_monthly']
             df['date'] = df['date'].apply(lambda _d: datetime.datetime.strftime(_d, "%Y%m%d"))
+        else:
+            logging.fatal("Unknow perird "+str(period))
+            exit()
 
         # check df size after resample
         if (df.__len__() < min_sample_f):
